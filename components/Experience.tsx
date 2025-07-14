a768d830c5376918334960bdd3e6684564bff408
Urmi03 <urmi.kothari@sigmainfo.net>
2025-07-15 00:36:04 +0530

    Fixed version for artifacts

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -39,6 +39,7 @@ const Experience = () => {
           {
             x: 0,
             opacity: 1,
+            
             duration: 0.8,
             delay: 0.3,
             ease: "power2.out",
