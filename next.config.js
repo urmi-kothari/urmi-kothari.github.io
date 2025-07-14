dd872647af07060eaf35c61a227628a2643a2290
Urmi03 <urmi.kothari@sigmainfo.net>
2025-07-15 00:39:50 +0530

    Added a export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',  // Enable static exports
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: '',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
